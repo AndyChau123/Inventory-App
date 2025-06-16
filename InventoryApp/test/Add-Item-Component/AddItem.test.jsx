23d242f013a35c5e6ae4aecf4aa7b494551f3ed1
AndyChau123 <achau2004@csu.fullerton.edu>
2025-06-16 09:50:11 -0700

    figuring out last 2 unit tests

--- InventoryApp/test/Add-Item-Component/AddItem.test.jsx
+++ InventoryApp/test/Add-Item-Component/AddItem.test.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import AddItem from "../../src/Add-Item-Component/AddItem";
 import { describe, it, expect } from "vitest";
 import userEvent from "@testing-library/user-event";
@@ -39,15 +39,62 @@ describe("AddItem", () => {
         expect(itemListValues).toHaveTextContent("This is my Storage");
     });
 
-/*
+
     it("changes display on screen user user clicks submit and some input fields are typed", async() => {
+        render(<AddItem />);
+
+        const button = screen.getByRole("button", {name: /Submit/i });
+
+        const itemName = screen.getByTestId("input-itemName");
+        const expDate = screen.getByTestId("input-expDate");
+        const location = screen.getByTestId("input-location");
+
+        await userEvent.type(itemName, "Milk");
+        await userEvent.type(expDate, "2025-07-21");
+        await userEvent.type(location, "Locker");
         
+        await userEvent.click(button);
+        const itemListValues = screen.getByTestId("displayItemList");
+
+        expect(itemListValues).toHaveTextContent("Milk");
+        expect(itemListValues).toHaveTextContent("2025-07-21");
+        expect(itemListValues).toHaveTextContent("Locker");
     });
-*/
+
 
     it("doesn't change display on screen when user clicks no input fields are typed", async () => {
         render(<AddItem />);
 
-        
+        const button = screen.getByRole("button", { name: /Submit/i });
+
+        const itemName = screen.getByTestId("input-itemName");
+        const quantity = screen.getByTestId("input-quantity");
+        const expDate = screen.getByTestId("input-expDate");
+        const useWithin = screen.getByTestId("input-useWithin");
+        const price = screen.getByTestId("input-price");
+        const location = screen.getByTestId("input-location");
+        const comments = screen.getByTestId("input-comments");
+
+        await userEvent.type(itemName, "");
+        await userEvent.type(quantity, "");
+        await userEvent.type(expDate, "");
+        await userEvent.type(useWithin, "");
+        await userEvent.type(price, "");
+        await userEvent.type(location, "");
+        await userEvent.type(comments, "");
+    
+        await userEvent.click(button);
+        const itemListValues = screen.getByTestId("displayItemList");
+
+        expect(itemListValues).toHaveTextContent("");
+        expect(itemListValues).toHaveTextContent("");
+        expect(itemListValues).toHaveTextContent("");
+        expect(itemListValues).toHaveTextContent("");
+        expect(itemListValues).toHaveTextContent("");
+        expect(itemListValues).toHaveTextContent("");
+        expect(itemListValues).toHaveTextContent("");
+
+        //Maybe instead of checking if user inputs nothing into field, check if button does nothing when user
+        //types in nothing in the input boxes.
     });
 });
