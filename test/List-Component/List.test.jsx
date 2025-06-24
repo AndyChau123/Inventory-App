1a2d4543a059341e18e1424e546ae0f9261b87b4
AndyChau123 <achau2004@csu.fullerton.edu>
2025-06-24 14:40:28 -0700

    added unit test for Select Items

--- test/List-Component/List.test.jsx
+++ test/List-Component/List.test.jsx
@@ -1,10 +1,10 @@
-import { describe, it, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { describe, it, expect, vi } from "vitest";
+import { render, screen, fireEvent } from "@testing-library/react";
 import React from "react";
 import List from "../../src/List-Component/List.jsx";
 import "@testing-library/jest-dom/vitest";
 
-describe("App", () => {
+describe("List", () => {
   it("Checks if sample items in itemList are being displayed in the table", async () => {
     // make sample items list to pass to itemList prop
     const sampleItems = [
@@ -40,5 +40,43 @@ describe("App", () => {
     expect(table[1]).toHaveTextContent("2.00");
     expect(table[1]).toHaveTextContent("2025-07-25");
     expect(table[1]).toHaveTextContent("Pantry");
+  });
+
+  it("checks to see if the checkboxes are being checked or not", async () => {
+      
+    const sampleItems = [
+      {
+        itemName: "Eggs",
+        quantity: "12",
+        price: "3.50",
+        expDate: "2025-08-01",
+        location: "Fridge",
+      },
+      {
+        itemName: "Bread",
+        quantity: "1",
+        price: "2.00",
+        expDate: "2025-07-25",
+        location: "Pantry",
+      },
+    ];
+
+    render(<List itemList={sampleItems}/>);
+
+    const checkboxes = screen.getAllByRole("checkbox");
+
+    //check the first three rows in table
+    expect(checkboxes[1]).not.toBeChecked();
+    expect(checkboxes[2]).not.toBeChecked();
+    expect(checkboxes[3]).not.toBeChecked();
+
+    fireEvent.click(checkboxes[1]);
+    fireEvent.click(checkboxes[2]);
+    fireEvent.click(checkboxes[3]);
+
+    expect(checkboxes[1]).toBeChecked();
+    expect(checkboxes[2]).toBeChecked();
+    expect(checkboxes[3]).toBeChecked();
+
   });
 });
